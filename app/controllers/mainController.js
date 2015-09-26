4442b5fd5ef983eab82e23b17d11040bceedfe87
Raul Rivero <raul@inceptures.com>
2015-09-26 14:17:16 -0400

    added perform basic math operations

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,9 +1,9 @@
-'use strict';
-
 angular.module('controllers', []).controller('View1Ctrl', function($scope) {
 
   $scope.mainInput = "0";
-  $scope.nums = [['(', ')', '%', 'CE'], [9, 8, 7, '÷'], [6, 5, 4, 'x'], [3, 2, 1, '-'], [0, '.', '=', '+']];
+  $scope.internalInput = "0";
+  $scope.ans = "";
+  $scope.nums = [['(', ')', '%', 'AC'], [9, 8, 7, '÷'], [6, 5, 4, 'x'], [3, 2, 1, '-'], [0, '.', '=', '+']];
 
   $scope.isChar = function (num) {
     if (num == '.')
@@ -15,41 +15,52 @@ angular.module('controllers', []).controller('View1Ctrl', function($scope) {
     console.log(key);
     switch (key) {
       case '+': {
+        $scope.internalInput += key;
         $scope.mainInput += key;
         break;
       }
       case '-': {
+        $scope.internalInput += key;
         $scope.mainInput += key;
         break;
       }
       case 'x': {
+        $scope.internalInput += '*';
         $scope.mainInput += key;
         break;
       }
       case '÷': {
+        $scope.internalInput += '/';
         $scope.mainInput += key;
         break;
       }
       case '%': {
-
-        break;
-      }
-      case '=': {
-
-        break;
-      }
-      case '.': {
+        $scope.internalInput += key;
         $scope.mainInput += key;
         break;
       }
-      case 'CE': {
+      case '=': {
+        $scope.ans = $scope.mainInput;
+        $scope.mainInput = eval($scope.internalInput);
+        break;
+      }
+      case '.': {
+        $scope.internalInput += key;
+        $scope.mainInput += key;
+        break;
+      }
+      case 'AC': {
+        $scope.internalInput = "0";
+        $scope.ans = "";
         $scope.mainInput = "0";
         break;
       }
       default : {
         if ($scope.mainInput == "0") {
           $scope.mainInput = "";
+          $scope.internalInput = "";
         }
+        $scope.internalInput += key;
         $scope.mainInput += key;
         break;
       }
