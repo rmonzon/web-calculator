9c4a6564e8b16711ef2c9ca6db81025d1de118bd
Raul Rivero <raul@inceptures.com>
2016-08-24 12:27:27 -0400

    some improvements

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,69 +1,83 @@
 angular.module('controllers', []).controller('View1Ctrl', function($scope) {
 
-  $scope.mainInput = "0";
-  $scope.internalInput = "0";
-  $scope.ans = "";
-  $scope.nums = [['(', ')', '%', 'AC'], [9, 8, 7, '÷'], [6, 5, 4, 'x'], [3, 2, 1, '-'], [0, '.', '=', '+']];
+    $scope.mainInput = "0";
+    $scope.internalInput = "0";
+    $scope.ans = "";
+    $scope.nums = [['(', ')', '%', 'AC'], [9, 8, 7, '÷'], [6, 5, 4, 'x'], [3, 2, 1, '-'], [0, '.', '=', '+']];
 
-  $scope.isChar = function (num) {
-    if (num == '.')
-      return false;
-    return typeof num != 'number';
-  };
+    $scope.isChar = function (num) {
+        if (num == '.')
+            return false;
+        return typeof num != 'number';
+    };
 
-  $scope.onClickButton = function (key) {
-    console.log(key);
-    switch (key) {
-      case '+': {
-        $scope.internalInput += key;
-        $scope.mainInput += key;
-        break;
-      }
-      case '-': {
-        $scope.internalInput += key;
-        $scope.mainInput += key;
-        break;
-      }
-      case 'x': {
-        $scope.internalInput += '*';
-        $scope.mainInput += key;
-        break;
-      }
-      case '÷': {
-        $scope.internalInput += '/';
-        $scope.mainInput += key;
-        break;
-      }
-      case '%': {
-        $scope.internalInput += key;
-        $scope.mainInput += key;
-        break;
-      }
-      case '=': {
-        $scope.ans = $scope.mainInput;
-        $scope.mainInput = eval($scope.internalInput);
-        break;
-      }
-      case '.': {
-        $scope.internalInput += key;
-        $scope.mainInput += key;
-        break;
-      }
-      case 'AC': {
-        $scope.internalInput = "0";
-        $scope.ans = "";
-        $scope.mainInput = "0";
-        break;
-      }
-      default : {
-        if ($scope.mainInput == "0") {
-          $scope.mainInput = "";
-          $scope.internalInput = "";
+    $scope.onClickButton = function (key) {
+        processKeyInput(key);
+    };
+
+    document.body.addEventListener("keypress", function (e) {
+        console.log();
+        if (e.which >= 48 && e.which <= 57 || e.which === 42 || e.which === 43 || e.which === 45 || e.which === 47) {
+            processKeyInput(e.key);
+            $scope.$apply();
+            return;
         }
-        $scope.internalInput += key;
-        $scope.mainInput += key;
-        break;
-      }
+        console.log(e.key, e.which);
+        e.preventDefault();
+    }, false);
+
+    function processKeyInput(key) {
+        switch (key) {
+            case '+': {
+                $scope.internalInput += key;
+                $scope.mainInput += key;
+                break;
+            }
+            case '-': {
+                $scope.internalInput += key;
+                $scope.mainInput += key;
+                break;
+            }
+            case 'x': {
+                $scope.internalInput += '*';
+                $scope.mainInput += key;
+                break;
+            }
+            case '÷': {
+                $scope.internalInput += '/';
+                $scope.mainInput += key;
+                break;
+            }
+            case '%': {
+                $scope.internalInput += key;
+                $scope.mainInput += key;
+                break;
+            }
+            case '=': {
+                $scope.ans = $scope.mainInput;
+                $scope.mainInput = eval($scope.internalInput);
+                break;
+            }
+            case '.': {
+                $scope.internalInput += key;
+                $scope.mainInput += key;
+                break;
+            }
+            case 'AC': {
+                $scope.internalInput = "0";
+                $scope.ans = "";
+                $scope.mainInput = "0";
+                break;
+            }
+            default : {
+                if ($scope.mainInput == "0") {
+                    $scope.mainInput = "";
+                    $scope.internalInput = "";
+                }
+                $scope.internalInput += key;
+                $scope.mainInput += key;
+                break;
+            }
+        }
     }
-  };
 });
